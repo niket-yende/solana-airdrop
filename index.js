d02caccb075d3e68ce94f8160177ecee21faf083
Niket Yende <niket6@gmail.com>
2024-01-24 19:36:07 +0530

    Add index script to airdrop sol to master address

---
+++ index.js
@@ -0,0 +1,30 @@
+// Importing required libraries 
+const cron = require("node-cron"); 
+const express = require("express"); 
+const { Connection, PublicKey, LAMPORTS_PER_SOL } = require("@solana/web3.js");
+
+const master_address = "9b9wKu2EFnk3ZHHhDaudfxpqtDM2SM3uJL54dMpmXTYw";
+  
+app = express(); // Initializing app 
+  
+// Creating a cron job which runs on every 30 minutes
+cron.schedule("*/30 * * * *", function() { 
+    console.log("running a task every 30 min");
+    airdrop(); 
+}); 
+
+airdrop = async () => {
+    try {
+        console.log(`Airdropping sol to ${master_address}`);
+        const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+        const myAddress = new PublicKey(master_address);
+        const signature = await connection.requestAirdrop(myAddress, LAMPORTS_PER_SOL);
+        await connection.confirmTransaction(signature);
+        console.log('Airdrop complete');
+    } catch(error) {
+        console.error('Error caught during airdrop ', error);
+    } 
+};
+
+
+app.listen(3000); 
